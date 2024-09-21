a9ac25acd503db702f1741f68c993af31cb2cb3b
ArunaArivudainambi <sujitha.2906@gmail.com>
2024-09-21 18:25:17 +0400

    Add files via upload

---
+++ Week1.Day2/SeleniumBrowser.java
@@ -0,0 +1,13 @@
+package week1.day2;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class SeleniumBrowser {
+
+	public static void main(String[] args) {
+		ChromeDriver bt= new ChromeDriver();
+		bt.get("https://www.google.com");
+		//bt.launchBrowser("Chrome");
+		//bt.loadUrl();
+
+	}
+
+}
